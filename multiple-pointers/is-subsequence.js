2fa0377eb0c5c9cd3f3c1ff25a828b0451d24db8
Toushif <toushif.haq@gmail.com>
2021-10-13 01:23:48 +0530

    average pair and is-subsequence

---
+++ multiple-pointers/is-subsequence.js
@@ -0,0 +1,35 @@
+// Multiple Pointers - isSubsequence
+// Write a function called isSubsequence which takes in two strings and checks
+// whether the characters in the first string form a subsequence of the characters
+// in the second string. In other words, the function should check whether the characters
+// in the first string appear somewhere in the second string, without their order changing.
+
+// Your solution MUST have AT LEAST the following complexities:
+// Time Complexity - O(N + M)
+// Space Complexity - O(1)
+
+// My solution
+function isSubsequence(pattern, str) {
+    if (str.length < pattern.length) return false;
+
+    let j = 0;
+    for (let i = 0; i < str.length; i++) {
+        if (pattern[j] === str[i]) {
+            j++;
+        }
+    }
+    return j === pattern.length;
+}
+
+// Recursive solution, but not O(n) space
+function isSubsequence(str1, str2) {
+    if (str1.length === 0) return true;
+    if (str2.length === 0) return false;
+    if (str2[0] === str1[0]) return isSubsequence(str1.slice(1), str2.slice(1));
+    return isSubsequence(str1, str2.slice(1));
+}
+
+console.log(isSubsequence("sing", "sting")); // true
+console.log(isSubsequence("abc", "abracadabra")); // true
+console.log(isSubsequence("abc", "acb")); // false (order matters)
+console.log(isSubsequence("", "cat")); // true
