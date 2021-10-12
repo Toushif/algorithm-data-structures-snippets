75bc5cb07b2b2b25d5a4170b4e9dc5e0b93f59a1
Toushif <toushif.haq@gmail.com>
2021-10-13 00:21:50 +0530

    binary matrix edit

--- binary-matrix-1.js
+++ binary-matrix-1.js
@@ -1,4 +1,12 @@
-//Binary matrix - https://www.tutorialspoint.com/distance-of-nearest-0-in-binary-matrix-in-javascript (Problem statement)
+//Binary matrix
+// write a JavaScript function that takes in a binary matrix as the only argument.
+
+// Our function should create a new matrix containing the same number of rows and columns,
+// and for each element of the original matrix the resulting matrix should contain that element's
+// nearest distance from 0 in the original matrix.
+
+// We have to keep in mind that while calculating distance it can move either horizontally or vertically
+// and not diagonally. And it's guaranteed that the matrix contains at least one 0.
 
 findNearestDistance = (arr = []) => {
     let array = [];
@@ -51,19 +59,23 @@ findNearestDistance = (arr = []) => {
     return res;
 };
 
-findNearestDistance([
-    [1, 1, 0],
-    [1, 1, 0],
-    [1, 1, 1],
-]);
+console.log(
+    findNearestDistance([
+        [1, 1, 0],
+        [1, 1, 0],
+        [1, 1, 1],
+    ])
+);
 
-Output - [2, 1, 0], [2, 1, 0], [3, 2, 1];
+// Output - [2, 1, 0], [2, 1, 0], [3, 2, 1];
 
-findNearestDistance([
-    [0, 1, 1, 0],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [0, 1, 1, 0],
-]);
+console.log(
+    findNearestDistance([
+        [0, 1, 1, 0],
+        [1, 1, 1, 1],
+        [1, 1, 1, 1],
+        [0, 1, 1, 0],
+    ])
+);
 
-Output - [0, 1, 1, 0], [1, 2, 2, 1], [1, 2, 2, 1], [0, 1, 1, 0];
+// Output - [0, 1, 1, 0], [1, 2, 2, 1], [1, 2, 2, 1], [0, 1, 1, 0];
