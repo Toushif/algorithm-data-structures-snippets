b4fd6854f6681d5610e4fc17afcefee834580d43
Toushif <toushif.haq@gmail.com>
2021-10-20 04:01:35 +0530

    Pascal triangle

---
+++ pascal-triangle.js
@@ -0,0 +1,34 @@
+// Given numRows, generate the first numRows of Pascal's triangle.
+// Pascal's triangle : To generate A[C] in row R, sum up A'[C] and A'[C-1] from previous row R - 1.
+
+// Example:
+
+// Given numRows = 5,
+
+// Return
+
+// [
+//      [1],
+//      [1,1],
+//      [1,2,1],
+//      [1,3,3,1],
+//      [1,4,6,4,1]
+// ]
+
+// My solution
+function pascal(A) {
+    let arr = [];
+    for (let i = 0; i < A; i++) {
+        let temp = [];
+        for (let j = 0; j <= i; j++) {
+            if (j === 0 || j === i) {
+                temp.push(1);
+            } else {
+                const sum = arr[i - 1][j - 1] + arr[i - 1][j];
+                temp.push(sum);
+            }
+        }
+        arr.push(temp);
+    }
+    return arr;
+}
