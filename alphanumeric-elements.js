4a88cc155f479539412686cc9d6e69fada25048b
Toushif <toushif.haq@gmail.com>
2021-10-26 21:50:26 +0530

    alphanumeric elements

---
+++ alphanumeric-elements.js
@@ -0,0 +1,13 @@
+// Write an algorithm that takes an array of string and returns a new array containing only alphanumeric elements.
+function alphaNumeric(arr) {
+    let store = [],
+        regex = /^(?=.*[a-zA-Z])(?=.*[0-9])[a-zA-Z0-9]+$/
+    arr.forEach(v => {
+        if(regex.test(v)) {
+            store.push(v);
+        }
+    })
+    return store
+}
+
+console.log(alphaNumeric(["190ab","ui", "90AJ", "PN", "UI19"])) // [ '190ab', '90AJ', 'UI19' ]
