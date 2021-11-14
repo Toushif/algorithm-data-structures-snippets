1c0141afef7c67bcbd2d8b6190da26530e394a5b
Toushif <toushif.haq@gmail.com>
2021-11-15 01:18:09 +0530

    only constructor

--- only-constructor.js
+++ only-constructor.js
@@ -4,7 +4,7 @@ function onlyCons(a) {
     //method then this constructor points to the function
     //itself, so onlyCons becomes equals to this.constructor
 
-    if (this.constructor !== onlyCons) {
+    if (this.constructor !== onlyCons) { //OR, this instanceof onlyCons
         throw "Cannot call as a function";
     }
 
@@ -13,3 +13,4 @@ function onlyCons(a) {
 
 //Here in the above function, we do not allow the function to be invoked normally.
 //Such functions are constructor functions. We can only create objects using these functions.
+onlyCons()
