d55aa5357747eea133c8327af581fc19e7ba7677
Toushif <toushif.haq@gmail.com>
2021-11-15 04:32:29 +0530

    added blobs, url and base64

---
+++ blob-url-base64.js
@@ -0,0 +1,35 @@
+// https://www.javatpoint.com/javascript-blob
+
+// Example 1
+var blob = new Blob(["Javatpoint"], { type: "text/plain" });
+var def = new FileReader();
+def.addEventListener("loadend", function (e) {
+    document.getElementById("main").innerHTML =
+        e.srcElement.result + blob.type + blob.size; //Read more aboue blob
+});
+def.readAsText(blob);
+
+// JavaScript Blob URL's
+let abc = new Blob(["Javatpoint"], { type: "text/plain" });
+var a = document.createElement("a");
+var fileName = "hello.txt";
+a.setAttribute("download", fileName);
+a.setAttribute("href", URL.createObjectURL(abc));
+document.body.appendChild(a);
+a.click();
+document.body.removeChild(a);
+
+// Blob to base64
+var blob = new Blob(['one,two,three'], { type: 'text/csv' });
+var a = document.createElement("a");
+var fileName = "hello.txt";
+a.setAttribute("download", fileName);
+document.body.appendChild(a);
+
+let reader = new FileReader();
+reader.readAsDataURL(blob);
+reader.onload = function () {
+    a.href = reader.result; // data url
+    a.click(); //this will download a csv file
+    document.body.removeChild(a);
+};
