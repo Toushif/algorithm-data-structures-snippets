9b93b31feeb6a87ffec7a1b9459cca3a72639ba9
Toushif <toushif.haq@gmail.com>
2021-11-18 04:57:49 +0530

    recursion edit

--- recursion/helper-method-recursion.js
+++ recursion/helper-method-recursion.js
@@ -38,4 +38,4 @@ function collectOddValues(arr){
     return newArr;
 }
 
-collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+console.log(collectOddValues([1, 2, 3, 4, 5, 6, 7, 8, 9])); //[ 1, 3, 5, 7, 9 ]
