d845a71019449b22012468117e319c2412b42f37
Toushif <toushif.haq@gmail.com>
2021-12-05 01:23:52 +0530

    only cons

--- only-constructor.js
+++ only-constructor.js
@@ -1,20 +1,20 @@
-// function onlyCons(a) {
-//     console.log(this.constructor, onlyCons); //initially this.constructor will point to the global or immediate
-//     //lexical scope object. But when called using new
-//     //method then this constructor points to the function
-//     //itself, so onlyCons becomes equals to this.constructor
+function onlyCons(a) {
+    console.log(this.constructor, onlyCons); //initially this.constructor will point to the global or immediate
+    //lexical scope object. But when called using new
+    //method then this constructor points to the function
+    //itself, so onlyCons becomes equals to this.constructor
 
-//     if (this.constructor !== onlyCons) { //OR, this instanceof onlyCons
-//         throw "Cannot call as a function";
-//     }
-//     // OR
-//     // if (!(this instanceof onlyCons)) {
-//     //     throw "Cannot call as a function";
-//     // }
+    if (this.constructor !== onlyCons) { //OR, this instanceof onlyCons
+        throw "Cannot call as a function";
+    }
+    // OR
+    // if (!(this instanceof onlyCons)) {
+    //     throw "Cannot call as a function";
+    // }
 
-//     this.a = a;
-// }
+    this.a = a;
+}
 
-// //Here in the above function, we do not allow the function to be invoked normally.
-// //Such functions are constructor functions. We can only create objects using these functions.
-// onlyCons()
+//Here in the above function, we do not allow the function to be invoked normally.
+//Such functions are constructor functions. We can only create objects using these functions.
+onlyCons()
