ee403a3318f143eaf7aa7d57c350e4ae2c2de214
Toushif <toushif.haq@gmail.com>
2021-12-04 07:38:06 +0530

    closure-2 added

---
+++ closures-2.js
@@ -0,0 +1,9 @@
+// What will this code print?
+
+for (let i = 0; i < 5; i++) {
+    setTimeout(function () {
+        console.log(i);
+    }, i * 1000);
+}
+
+// It will print 0 1 2 3 4, because we use let instead of var here. The variable i is only seen in the for loop’s block scope.
