7fe853b65f6ad4aa58045cb48a3917d1744d8c0b
Toushif <toushif.haq@gmail.com>
2021-12-04 07:40:27 +0530

    js-snippet-6 added

--- js-snippet-6.js
+++ js-snippet-6.js
@@ -1,7 +1,7 @@
 // What do the following lines output, and why? 
 
-console.log(1 < 2 < 3); 
-console.log(3 > 2 > 1);
+console.log(1 < 2 < 3); // true
+console.log(3 > 2 > 1); // false
 
 // he first statement returns true which is as expected.
 
