0761d0cac001d6fd95c0c18d9fc0cb280376ebae
Toushif <toushif.haq@gmail.com>
2021-12-04 07:28:30 +0530

    js-snippet-4 added

--- js-snippet-4.js
+++ js-snippet-4.js
@@ -16,3 +16,17 @@
 // undefined
 // 2
 
+// var statements are hoisted (without their value initialization) to the top of the global or function scope it belongs to, even when it’s inside a with or catch block. However, the error’s identifier is only visible inside the catch block. It is equivalent to:
+
+(function () {
+    var x, y; // outer and hoisted
+    try {
+        throw new Error();
+    } catch (x /* inner */) {
+        x = 1; // inner x, not the outer one
+        y = 2; // there is only one y, which is in the outer scope
+        console.log(x /* inner */);
+    }
+    console.log(x);
+    console.log(y);
+})();
