7eedf5c1e5f3723f95d9e86f264a21f93a358304
Toushif <toushif.haq@gmail.com>
2021-12-04 07:53:26 +0530

    closures-2 changed

--- closures-2.js
+++ closures-2.js
@@ -7,3 +7,13 @@ for (let i = 0; i < 5; i++) {
 }
 
 // It will print 0 1 2 3 4, because we use let instead of var here. The variable i is only seen in the for loop’s block scope.
+
+// With var we can do this -
+
+for (var i = 0; i < 5; i++) {
+    (function (x) {
+        setTimeout(function () {
+            console.log(x);
+        }, x * 1000);
+    })(i);
+}
