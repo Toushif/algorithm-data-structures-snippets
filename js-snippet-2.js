c4395fd5ad88c639a97b475db8bca1db64ebf160
Toushif <toushif.haq@gmail.com>
2021-12-04 05:39:13 +0530

    js-snippet-2 modified

--- js-snippet-2.js
+++ js-snippet-2.js
@@ -7,3 +7,7 @@ console.log(+"1" + "1" + "2"); // '112'
 console.log("A" - "B" + "2"); // 'NaN2'
 console.log("A" - "B" + 2); // NaN
 
+console.log("0 || 1 = " + (0 || 1)); // 0 || 1 = 1
+console.log("1 || 2 = " + (1 || 2)); // 1 || 2 = 1
+console.log("0 && 1 = " + (0 && 1)); // 0 && 1 = 0
+console.log("1 && 2 = " + (1 && 2)); // 1 && 2 = 2
