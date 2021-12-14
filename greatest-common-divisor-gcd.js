a07281937a4e282f5d3cfc3db609ca350faffe3a
Toushif <toushif.haq@gmail.com>
2021-12-15 04:08:35 +0530

    added gcd

---
+++ greatest-common-divisor-gcd.js
@@ -0,0 +1,18 @@
+function gcd(num1, num2) {
+    const min = Math.min(num1, num2),
+        max = num1 === min ? num2 : num1;
+    if (max % min === 0) return min;
+    let i = 2,
+        res = 1;
+    while (i <= min / 2) {
+        if (num1 % i === 0 && num2 % i === 0) {
+            res = i;
+        }
+        i++;
+    }
+    return res;
+}
+
+console.log(gcd(60, 40))
+console.log(gcd(300, 100))
+console.log(gcd(197, 71))
