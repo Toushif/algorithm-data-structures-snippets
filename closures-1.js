f0d57760e60794d1083e24808e362caceaed3d84
Toushif <toushif.haq@gmail.com>
2022-03-21 07:09:46 +0530

    closures 1

--- closures-1.js
+++ closures-1.js
@@ -12,7 +12,7 @@ for (var i = 0; i < 5; i++) {
 // (a) What gets logged to the console when the user clicks on “Button 4” and why?
 // (b) Provide one or more alternate implementations that will work as expected.
 
-// Above the output will always be 5 since var i is a global variable and its value is lost after iteration within the fucntion scope, so the value of i is 5 which gets printed.
+// Above the output will always be 5 since var i is a global variable and its value is lost after iteration within the block scope, so the value of i is 5 which gets printed.
 // At the point that the onclick method is invoked (for any of the buttons), the for loop has already completed and the variable i already has a value of 5. (Bonus points for the interviewee if they know enough to talk about how execution contexts, variable objects, activation objects, and the internal “scope” property contribute to the closure behavior.)
 
 // 4 solutions -
@@ -44,7 +44,7 @@ for (var i = 0; i < 5; i++) {
     document.body.appendChild(btn);
 }
 
-// Using ES^ block scope let instead of var for variable declaration -
+// Using ES6 block scope let instead of var for variable declaration -
 for (let i = 0; i < 5; i++) {
     var btn = document.createElement("button");
     btn.appendChild(document.createTextNode("Button " + i));
